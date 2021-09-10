6a100f351afdb77b893950245be7249e9e66d5a0
Md. Ahosan Habib <cseahosan@gmail.com>
2021-09-11 00:28:47 +0600

    refactoring-2 table header

--- src/components/movies-table.component.js
+++ src/components/movies-table.component.js
@@ -1,43 +1,13 @@
 import React from "react";
+import Table from "../common/table.component";
 
 export default function MoviesTable({ movies }){
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Poster</th>
-          <th scope="col">Title</th>
-          <th scope="col">Rating</th>
-          <th scope="col">Your Rating</th>
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map((movie) => {
-          return (
-            <tr>
-              <th scope="row">
-                <img
-                  style={{ width: "30px", height: "auto" }}
-                  src={movie.posterurl}
-                  alt="imag"
-                />
-              </th>
-              <td>{movie.title}</td>
-              <td>
-                <i class="bi bi-star"></i>
-                {movie.imdbRating}
-              </td>
-              <td>
-                {movie.your_rating ? (
-                  <i class="bi bi-star-fill"></i>
-                ) : (
-                  <i class="bi bi-star"></i>
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  const columns = [
+    {path: 'Poster', label: "Poster"},
+    {path: 'Title', label: "Title"},
+    {path: 'Poster', label: "Rating"},
+    {path: 'Your Rating ', label: "Your Rating"}
+  ];
+
+  return <Table columns={columns} items = {movies} />;
 };
