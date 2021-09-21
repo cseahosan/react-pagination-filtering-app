f19724917dc5cd43b0a4ff1c31a6d5573b45f595
Md. Ahosan Habib <cseahosan@gmail.com>
2021-09-21 16:40:08 +0600

    working on login form

--- src/components/login.component.jsx
+++ src/components/login.component.jsx
@@ -1,28 +1,74 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
-  // state = {
-  //     user: { username: '', password: ''}
-  // }
+  state = {
+    user: { username: "", password: "" },
+  };
 
-  // handleSubmit = e => {
-  //     e.preventDefault();
-  //     const userName = e.target[0].value;
-  //     const password = e.target[1].value;
-  //     console.log(userName, password);
-  // }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const userName = e.target[0].value;
+    const password = e.target[1].value;
+    console.log(userName, password);
+  };
 
-  // handleChange = e => {
-  //     const name = e.currentTarget.name;
-  //     const value = e.target.value;
+  handleChange = (e) => {
+    const name = e.currentTarget.name;
+    const value = e.target.value;
 
-  //     const updatedUser = {...this.state.user};
-  //     updatedUser[name] = value;
+    const updatedUser = { ...this.state.user };
+    updatedUser[name] = value;
 
-  //     this.setState({user: updatedUser});
-  // }
+    this.setState({ user: updatedUser });
+  };
 
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="container w-50 mx-auto">
+        <form onSubmit={this.handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              User Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+              onChange={this.handleChange}
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              onChange={(e) => this.handleChange(e)}
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
